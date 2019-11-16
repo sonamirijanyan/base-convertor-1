00211cbdefa3dcc4220007ad0b9fc9753fba744f
ArmFrPl <ehakobkekhvyan7@gmail.com>
2019-11-17 01:27:01 +0400

    fixed a small bug

--- converter.js
+++ converter.js
@@ -1,5 +1,5 @@
 function convertToBinary(num){
-  console.log(num.toString(2));
+  return num.toString(2);
 }
 
 function convertToHexa(num){
@@ -7,7 +7,8 @@ function convertToHexa(num){
   if (num < 0) {
     num += 0xFFFFFFFF + 1;
   }
-  console.log(num.toString(16).toUpperCase());
+
+  return (num.toString(16).toUpperCase());
 }
 
 module.exports = {
--- main.js
+++ main.js
@@ -5,9 +5,9 @@ const num = reader.questionInt("Please enter the number you want to convert = ")
 const opt = reader.questionInt("Please enter option: \n 1:Binary \n 2:Hexadecimal \n");
 
 if(opt === 1){
-  convertToBinary(num);
+  console.log(convertToBinary(num));
 }else if(opt === 2){
-  convertToHexa(num);
+  console.log(convertToHexa(num));
 }else{
   console.log("Imported value should be number");
 }
