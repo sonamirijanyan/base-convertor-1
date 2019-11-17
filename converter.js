053e862ddb658c6207352a7954c935066e4e31ff
ArmFrPl <ehakobkekhvyan7@gmail.com>
2019-11-17 17:57:01 +0400

    changed converting function

--- converter.js
+++ converter.js
@@ -1,14 +1,26 @@
-function convertToBinary(num){
-  return num.toString(2);
-}
-
-function convertToHexa(num){
+function convertToBase(num, base){
 
   if (num < 0) {
     num += 0xFFFFFFFF + 1;
   }
 
-  return (num.toString(16).toUpperCase());
+  let arr = [];
+  let count = -1;
+  let vals = ["A", "B", "C", "D", "E", "F"];
+
+  for(let i = num; i > 0; i = Math.floor(i / base)){
+    if(i % base >= 10) {
+      arr.push(vals[i % base - 10]);
+    } else {
+      arr.push(i % base);
+    }
+    count += 1;
+  }
+let result = '';
+  for (let j = count; j >= 0; j--) {
+    result += arr[j];
+  }
+  return result;
 }
 
 module.exports = {
--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const reader = require('readline-sync');
-const {convertToBinary, convertToHexa} = require('./converter');
+const {convertToBase} = require('./converter');
 
 const num = reader.questionInt("Please enter the number you want to convert = ");
 const opt = reader.questionInt("Please enter option: \n 1:Binary \n 2:Hexadecimal \n");
 
 if(opt === 1){
-  console.log(convertToBinary(num));
+  console.log(convertToBase(num, 2));
 }else if(opt === 2){
-  console.log(convertToHexa(num));
+  console.log(convertToBase(num, 16));
 }else{
   console.log("Imported value should be number");
 }
